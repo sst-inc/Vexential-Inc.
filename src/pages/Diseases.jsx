1f599dd8e3c02b30fd7f3d282abf0e0ca9b873b9
mushroomthatcodes <130353265+mushroomthatcodes@users.noreply.github.com>
2023-09-07 18:24:17 +0800

    extra work completed. some dynamic pages complete.
    Encountering an issue at this point where some rendering errors are happening. dunno why, kevin to fix.

--- src/pages/Diseases.jsx
+++ src/pages/Diseases.jsx
@@ -14,10 +14,12 @@ const DiseasesPage = () => {
 
   const [diseases, setDiseases] = useState([
     { id: 'diabetes', name: 'Diabetes', selected: false },
-    { id: 'hypertension', name: 'Hypertension', selected: false },
-    { id: 'heartDisease', name: 'Heart Disease', selected: false },
-    { id: 'stroke', name: 'Stroke', selected: false },
-    { id: 'lungDisease', name: 'Lung Disease', selected: false },
+    { id: 'highBloodPressure', name: 'High Blood Pressure', selected: false },
+    { id: 'lowBloodPressure', name: 'Low Blood Pressure', selected: false },
+    { id: 'cardiovascularDiseases', name: 'Cardiovascular Diseases', selected: false },
+    { id: 'highCholesterol', name: 'High Cholesterol', selected: false },
+    { id: 'arthritis', name: 'Arthritis', selected: false },
+    { id: 'dementia', name: 'Dementia', selected: false },
   ]);
 
   const toggleDisease = (diseaseId) => {
@@ -26,6 +28,13 @@ const DiseasesPage = () => {
         disease.id === diseaseId ? { ...disease, selected: !disease.selected } : disease
       )
     );
+  };
+
+  const handleNextPress = () => {
+    // You can perform any necessary actions here before navigating to the next screen
+
+    // Navigate to the MealPlan screen
+    navigation.navigate('MealPlan');
   };
 
   return (
@@ -41,26 +50,25 @@ const DiseasesPage = () => {
           Do you have any of these chronic diseases?
         </StyledText>
         <StyledView className="flex-col items-center">
-        {diseases.map((disease) => (
+          {diseases.map((disease) => (
             <StyledTouchableOpacity
-            key={disease.id}
-            className={`w-80 h-20 rounded-md bg-blue-500 ${
+              key={disease.id}
+              className={`w-80 h-16 rounded-md bg-blue-500 ${
                 disease.selected ? 'border border-white' : ''
-            } mt-2 flex-row items-center justify-center`}
-            onPress={() => toggleDisease(disease.id)}
+              } mt-2 flex-row items-center justify-center`}
+              onPress={() => toggleDisease(disease.id)}
             >
-            {disease.selected && (
+              {disease.selected && (
                 <StyledText className="text-white text-lg font-semibold mr-2">&#10003;</StyledText>
-            )}
+              )}
+              <StyledText className="text-white text-lg">{disease.name}</StyledText>
             </StyledTouchableOpacity>
-        ))}
+          ))}
         </StyledView>
 
         <StyledTouchableOpacity
           className="bg-blue-500 p-4 rounded-md absolute bottom-5 right-5"
-          onPress={() => {
-            // Handle button press, e.g., navigate to the next screen
-          }}
+          onPress={handleNextPress}
         >
           <StyledText className="text-white text-lg font-semibold">Next</StyledText>
         </StyledTouchableOpacity>
--- src/pages/RecommendedMealPlan.jsx
+++ src/pages/RecommendedMealPlan.jsx
@@ -1,8 +1,8 @@
-import React, { useState, useEffect } from 'react';
+import React from 'react';
 import { Text, View, Pressable, Image } from 'react-native';
 import { styled } from 'nativewind';
 import { useNavigation, useRoute } from '@react-navigation/native';
-
+import { foodChosenForBreakfast, foodChosenForLunch, foodChosenForDinner } from '../../App'
 
 const StyledView = styled(View);
 const StyledText = styled(Text);
@@ -11,7 +11,88 @@ const StyledPressable = styled(Pressable);
 const RecommendedMealPlan = () => {
   const navigation = useNavigation();
   const route = useRoute();
-  const { dayNumber, breakfast, lunch, dinner } = route.params;
+  const { dayNumber } = route.params;
+
+  // Define the meal data for each day
+  const mealData = {
+    1: {
+      breakfast: {
+        name: foodChosenForBreakfast.name,
+        image_uri:
+        foodChosenForBreakfast.image_uri,
+      },
+      lunch: {
+        name: foodChosenForLunch.name,
+        image_uri: foodChosenForLunch.image_uri,
+      },
+      dinner: {
+        name: foodChosenForDinner.name,
+        image_uri: foodChosenForDinner.image_uri,
+      },
+    },
+    2: {
+      breakfast: {
+        name: 'Greek Yogurt with Mixed Berries and Chopped Almonds',
+        image_uri:
+          'https://www.almonds.com/sites/default/files/2020-07/abc_BreakfastPairing_A_re01.jpg',
+      },
+      lunch: {
+        name: 'Lentil and Bell Pepper Stir-Fry with Tofu',
+        image_uri: 'https://example.com/lentil-stir-fry.jpg',
+      },
+      dinner: {
+        name: 'Bok Choy and Tofu Stir-Fry with Brown Rice',
+        image_uri: 'https://example.com/bok-choy-stir-fry.jpg',
+      },
+    },
+    3: {
+      breakfast: {
+        name: 'Oatmeal Pancakes with Blueberries',
+        image_uri:
+          'https://s23209.pcdn.co/wp-content/uploads/2012/10/IMG_6988-360x360.jpg',
+      },
+      lunch: {
+        name: 'Chicken and Asparagus Salad with Black Beans',
+        image_uri: 'https://example.com/chicken-salad.jpg',
+      },
+      dinner: {
+        name: 'Bell Pepper Stuffed with Lentils and Grilled Chicken',
+        image_uri: 'https://example.com/stuffed-pepper.jpg',
+      },
+    },
+    // Add more days ltr u stupid idiot adrill
+
+    4: {
+      breakfast:{
+        name: 'Whole wheat toast with poached/scrambled/fried eggs',
+        image_uri: 'https://images.squarespace-cdn.com/content/v1/55f9c9abe4b09e993da5f156/1539174364166-TIQK5VFA6ANJ2G8X028L/image-asset.png?format=2500w'
+      },
+
+      lunch: {
+        name: 'Black bean and mushroom soup with a side of mixed greens', 
+        image_uri: 'https://addapinch.com/wp-content/uploads/2017/10/mushroom-black-bean-soup-recipe-0020.jpg',
+      },
+
+      dinner: {
+        name: 'Lentil and turkey stir-fry with quinoa', 
+        image_uri: 'https://i0.wp.com/sepcooks.com/wp-content/uploads/2021/07/3DD96B95-F1C8-40D2-99F9-74B73EF16957_1_201_a.jpeg?zoom=2&resize=665%2C435&ssl=1',
+      }
+    },
+
+    5: {
+      breakfast:{
+        name: 'Quinoa porridge with mixed fruits', 
+        image_uri:'https://www.simplyquinoa.com/wp-content/uploads/2020/01/ultimate-superfood-quinoa-porridge-7.webp'
+      },
+
+      dinner: {
+        
+      }
+    }
+  };
+  
+
+  const selectedMeals = mealData[dayNumber];
 
   return (
     <StyledView className="flex-1 flex-col bg-blue-100 p-0">
@@ -65,11 +146,13 @@ const RecommendedMealPlan = () => {
           <StyledView className="w-[292px] h-[394px] rounded-lg bg-[#C4E5F8] border border-gray-300">
             {/* Content for Breakfast */}
             <StyledText className="text-black text-xl font-semibold p-4">
-              {breakfast ? breakfast.name : 'No breakfast'}
+              {selectedMeals.breakfast
+                ? selectedMeals.breakfast.name
+                : 'No breakfast'}
             </StyledText>
-            {breakfast && (
+            {selectedMeals.breakfast && (
               <Image
-                source={{ uri: breakfast.image_uri }}
+                source={{ uri: selectedMeals.breakfast.image_uri }}
                 style={{ width: '100%', height: 200 }}
                 resizeMode="cover"
               />
@@ -78,11 +161,11 @@ const RecommendedMealPlan = () => {
           <StyledView className="w-[292px] h-[394px] rounded-lg bg-[#C4E5F8] border border-gray-300">
             {/* Content for Lunch */}
             <StyledText className="text-black text-xl font-semibold p-4">
-              {lunch ? lunch.name : 'No lunch'}
+              {selectedMeals.lunch ? selectedMeals.lunch.name : 'No lunch'}
             </StyledText>
-            {lunch && (
+            {selectedMeals.lunch && (
               <Image
-                source={{ uri: lunch.image_uri }}
+                source={{ uri: selectedMeals.lunch.image_uri }}
                 style={{ width: '100%', height: 200 }}
                 resizeMode="cover"
               />
@@ -91,11 +174,11 @@ const RecommendedMealPlan = () => {
           <StyledView className="w-[292px] h-[394px] rounded-lg bg-[#C4E5F8] border border-gray-300">
             {/* Content for Dinner */}
             <StyledText className="text-black text-xl font-semibold p-4">
-              {dinner ? dinner.name : 'No dinner'}
+              {selectedMeals.dinner ? selectedMeals.dinner.name : 'No dinner'}
             </StyledText>
-            {dinner && (
+            {selectedMeals.dinner && (
               <Image
-                source={{ uri: dinner.image_uri }}
+                source={{ uri: selectedMeals.dinner.image_uri }}
                 style={{ width: '100%', height: 200 }}
                 resizeMode="cover"
               />
