07ede2a360bedbdca0ff228d505b78c3591be54f
Crypstick <88693462+Crypstick@users.noreply.github.com>
2023-09-09 11:20:05 +0800

    Meal Information + Introduction Page Saving
    added dynamic page for information for meal, requires more styling. Current version is having issues with saving data on startup pages, solving soon.

--- src/pages/Age.jsx
+++ src/pages/Age.jsx
@@ -3,52 +3,57 @@ import { View, Text, ImageBackground, TouchableOpacity } from 'react-native';
 import { styled } from 'nativewind';
 import { useNavigation } from '@react-navigation/native';
 
+
 const StyledView = styled(View);
 const StyledText = styled(Text);
 const StyledTouchableOpacity = styled(TouchableOpacity);
 
+
 const AgePage = () => {
-  const navigation = useNavigation();
+const navigation = useNavigation();
 
-  const handleAgeSelection = (ageRange) => {
-    // Navigate to the Diseases page with the selected age range
-    navigation.navigate('Diseases', { ageRange });
-  };
 
-  return (
-    <ImageBackground
-      source={require('../../assets/startup_background.png')}
-      style={{ flex: 1, resizeMode: 'cover', justifyContent: 'center', alignItems: 'center' }}
-    >
-      <StyledView className="flex flex-col items-center justify-center">
-        <StyledText className="text-black text-4xl font-semibold mb-6">What is your age?</StyledText>
-        <StyledTouchableOpacity
-          className="bg-blue-500 p-4 rounded-md mb-4"
-          onPress={() => handleAgeSelection('60-70')}
-        >
-          <StyledText className="text-white text-lg font-semibold">60 - 70</StyledText>
-        </StyledTouchableOpacity>
-        <StyledTouchableOpacity
-          className="bg-blue-500 p-4 rounded-md mb-4"
-          onPress={() => handleAgeSelection('70-80')}
-        >
-          <StyledText className="text-white text-lg font-semibold">70 - 80</StyledText>
-        </StyledTouchableOpacity>
-        <StyledTouchableOpacity
-          className="bg-blue-500 p-4 rounded-md mb-4"
-          onPress={() => handleAgeSelection('80-90')}
-        >
-          <StyledText className="text-white text-lg font-semibold">80 - 90</StyledText>
-        </StyledTouchableOpacity>
-        <StyledTouchableOpacity
-          className="bg-blue-500 p-4 rounded-md mb-4"
-          onPress={() => handleAgeSelection('90-100')}
-        >
-          <StyledText className="text-white text-lg font-semibold">90 - 100</StyledText>
-        </StyledTouchableOpacity>
-      </StyledView>
-    </ImageBackground>
-  );
+const handleAgeSelection = (ageRange) => {
+// Navigate to the Diseases page with the selected age range
+navigation.navigate('Diseases', { ageRange });
 };
+
+
+return (
+<ImageBackground
+source={require('../../assets/startup_background.png')}
+style={{ flex: 1, resizeMode: 'cover', justifyContent: 'center', alignItems: 'center' }}
+>
+<StyledView className="flex flex-col items-center justify-center">
+<StyledText className="text-black text-4xl font-semibold mb-6">What is your age?</StyledText>
+<StyledTouchableOpacity
+className="bg-blue-500 p-4 rounded-md mb-4"
+onPress={() => handleAgeSelection('60-70')}
+>
+<StyledText className="text-white text-lg font-semibold">60 - 70</StyledText>
+</StyledTouchableOpacity>
+<StyledTouchableOpacity
+className="bg-blue-500 p-4 rounded-md mb-4"
+onPress={() => handleAgeSelection('70-80')}
+>
+<StyledText className="text-white text-lg font-semibold">70 - 80</StyledText>
+</StyledTouchableOpacity>
+<StyledTouchableOpacity
+className="bg-blue-500 p-4 rounded-md mb-4"
+onPress={() => handleAgeSelection('80-90')}
+>
+<StyledText className="text-white text-lg font-semibold">80 - 90</StyledText>
+</StyledTouchableOpacity>
+<StyledTouchableOpacity
+className="bg-blue-500 p-4 rounded-md mb-4"
+onPress={() => handleAgeSelection('90-100')}
+>
+<StyledText className="text-white text-lg font-semibold">90 - 100</StyledText>
+</StyledTouchableOpacity>
+</StyledView>
+</ImageBackground>
+);
+};
+
 
 export default AgePage;
--- src/pages/Diseases.jsx
+++ src/pages/Diseases.jsx
@@ -1,80 +1,105 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import { View, Text, ImageBackground, TouchableOpacity } from 'react-native';
 import { styled } from 'nativewind';
 import { useNavigation, useRoute } from '@react-navigation/native';
+import AsyncStorage from '@react-native-async-storage/async-storage'; // Import AsyncStorage
+
 
 const StyledView = styled(View);
 const StyledText = styled(Text);
 const StyledTouchableOpacity = styled(TouchableOpacity);
 
+
 const DiseasesPage = () => {
-  const navigation = useNavigation();
-  const route = useRoute();
-  const { ageRange } = route.params;
+const navigation = useNavigation();
+const route = useRoute();
+const { ageRange } = route.params;
 
-  const [diseases, setDiseases] = useState([
-    { id: 'diabetes', name: 'Diabetes', selected: false },
-    { id: 'highBloodPressure', name: 'High Blood Pressure', selected: false },
-    { id: 'lowBloodPressure', name: 'Low Blood Pressure', selected: false },
-    { id: 'cardiovascularDiseases', name: 'Cardiovascular Diseases', selected: false },
-    { id: 'highCholesterol', name: 'High Cholesterol', selected: false },
-    { id: 'arthritis', name: 'Arthritis', selected: false },
-    { id: 'dementia', name: 'Dementia', selected: false },
-  ]);
 
-  const toggleDisease = (diseaseId) => {
-    setDiseases((prevDiseases) =>
-      prevDiseases.map((disease) =>
-        disease.id === diseaseId ? { ...disease, selected: !disease.selected } : disease
-      )
-    );
-  };
+const [diseases, setDiseases] = useState([
+{ id: 'diabetes', name: 'Diabetes', selected: false },
+{ id: 'highBloodPressure', name: 'High Blood Pressure', selected: false },
+// Add more diseases as needed
+]);
 
-  const handleNextPress = () => {
-    // You can perform any necessary actions here before navigating to the next screen
 
-    // Navigate to the MealPlan screen
-    navigation.navigate('MealPlan');
-  };
-
-  return (
-    <ImageBackground
-      source={require('../../assets/startup_background.png')}
-      style={{ flex: 1, resizeMode: 'cover', justifyContent: 'center', alignItems: 'center' }}
-    >
-      <StyledView className="flex flex-col items-center justify-center">
-        <StyledText className="text-black text-4xl font-semibold mb-6">
-          Selected Age Range: {ageRange}
-        </StyledText>
-        <StyledText className="text-black text-3xl text-center mb-6">
-          Do you have any of these chronic diseases?
-        </StyledText>
-        <StyledView className="flex-col items-center">
-          {diseases.map((disease) => (
-            <StyledTouchableOpacity
-              key={disease.id}
-              className={`w-80 h-16 rounded-md bg-blue-500 ${
-                disease.selected ? 'border border-white' : ''
-              } mt-2 flex-row items-center justify-center`}
-              onPress={() => toggleDisease(disease.id)}
-            >
-              {disease.selected && (
-                <StyledText className="text-white text-lg font-semibold mr-2">&#10003;</StyledText>
-              )}
-              <StyledText className="text-white text-lg">{disease.name}</StyledText>
-            </StyledTouchableOpacity>
-          ))}
-        </StyledView>
-
-        <StyledTouchableOpacity
-          className="bg-blue-500 p-4 rounded-md absolute bottom-5 right-5"
-          onPress={handleNextPress}
-        >
-          <StyledText className="text-white text-lg font-semibold">Next</StyledText>
-        </StyledTouchableOpacity>
-      </StyledView>
-    </ImageBackground>
-  );
+const toggleDisease = (diseaseId) => {
+setDiseases((prevDiseases) =>
+prevDiseases.map((disease) =>
+disease.id === diseaseId ? { ...disease, selected: !disease.selected } : disease
+)
+);
 };
+
+
+const selectedDiseases = diseases.filter((disease) => disease.selected);
+
+
+const handleNextPress = async () => {
+// Check if startup pages have been shown before
+const startupPagesShown = await AsyncStorage.getItem('startupPagesShown');
+
+
+if (!startupPagesShown) {
+// If startup pages haven't been shown, navigate to them
+navigation.navigate('Languages'); // Start with the first startup page
+} else {
+// If startup pages have been shown, navigate to MealPlan
+navigation.navigate('MealPlan', { selectedDiseases });
+}
+};
+
+
+return (
+<ImageBackground
+source={require('../../assets/startup_background.png')}
+style={{ flex: 1, resizeMode: 'cover', justifyContent: 'center', alignItems: 'center' }}
+>
+<StyledView className="flex flex-col items-center justify-center">
+<StyledText className="text-black text-4xl font-semibold mb-6">
+Selected Age Range: {ageRange}
+</StyledText>
+<StyledText className="text-black text-3xl text-center mb-6">
+Do you have any of these chronic diseases?
+</StyledText>
+<StyledView className="flex-col items-center">
+{diseases.map((disease) => (
+<StyledTouchableOpacity
+key={disease.id}
+className={`w-80 h-14 rounded-md bg-blue-500 ${
+disease.selected ? 'border border-white' : ''
+} mt-2 flex-row items-center justify-center`}
+onPress={() => toggleDisease(disease.id)}
+>
+{disease.selected && (
+<StyledText className="text-white text-lg font-semibold mr-2">&#10003;</StyledText>
+)}
+<StyledText className="text-white text-lg">{disease.name}</StyledText>
+</StyledTouchableOpacity>
+))}
+</StyledView>
+
+
+<StyledTouchableOpacity
+className="bg-blue-500 p-4 rounded-md absolute bottom-5 right-5"
+onPress={handleNextPress}
+>
+<StyledText className="text-white text-lg font-semibold">Next</StyledText>
+</StyledTouchableOpacity>
+</StyledView>
+</ImageBackground>
+);
+};
+
+
+
+
+
+
+
+
+
+
+
 
 export default DiseasesPage;
